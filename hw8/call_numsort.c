60fc1a1add5a61cad600363cab1f1b9f02dc8de1
Chun-Hung Tseng <henrybear327@gmail.com>
2015-12-27 18:44:17 +0800

    Hw8 ready for turnin

--- hw8/call_numsort.c
+++ hw8/call_numsort.c
@@ -4,6 +4,8 @@
 extern int *NumSort(int size, int array[]);
 extern int FileOutput(char *str);
 
+#define DEBUG 0
+
 int main()
 {
     int n;
@@ -11,7 +13,7 @@ int main()
 
     int array[n];
 
-    for(int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
         scanf("%d", &array[i]);
 
     int *result = NumSort(n, array);
@@ -19,19 +21,19 @@ int main()
     // get number string in reverse order
     char string[1000] = {0};
     int idx = 0;
-    for(int i = n - 1; i >= 0; i--) {
+    for (int i = n - 1; i >= 0; i--) {
         int curr = result[i];
-        while(curr) {
+        while (curr) {
             string[idx++] = (curr % 10) + '0';
             curr /= 10;
         }
-        if(i)
+        if (i)
             string[idx++] = ' ';
     }
 
-    //reverse the number string
+    // reverse the number string
     int l = 0, r = idx - 1;
-    while(l < r) {
+    while (l < r) {
         char tmp = string[l];
         string[l] = string[r];
         string[r] = tmp;
@@ -39,18 +41,25 @@ int main()
         r--;
     }
 
-    for(int i= 0; i < idx; i++)
+#if DEBUG == 1
+    for (int i = 0; i < n; i++)
+        printf("%d%c", result[i], i == n - 1 ? '\n' : ' ');
+
+    for (int i = 0; i < idx; i++)
         printf("%c", string[i]);
     printf("\n");
+#endif
 
     int error_code = FileOutput(string);
-    if(error_code == 0)
+    if (error_code == 0)
         printf("File created successfully\n");
     else
         printf("File creation failed. Error code %d\n", error_code);
 
-    for(int i = 0; i < n; i++)
-        printf("%d%c", result[i], i == n - 1 ? '\n' : ' ');
+    printf("The file should show: \n");
+    for (int i = 0; i < idx; i++)
+        printf("%c", string[i]);
+    printf("\n");
 
     return 0;
 }
